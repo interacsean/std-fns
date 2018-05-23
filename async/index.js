55bfafbc866bfcb88559413b2a08eb9d6f0f661f
Daniel Martinez <dandro.dsign@gmail.com>
2018-05-23 22:36:52 +1000

    Fix esports error

--- async/index.js
+++ async/index.js
@@ -2,6 +2,6 @@ const bimap = require('./bimap/bimap');
 const map = require('./map/map');
 const tap = require('./tap/tap');
 
-module.exports = bimap;
-module.exports = map;
-module.exports = tap;
+module.exports.bimap = bimap;
+module.exports.map = map;
+module.exports.tap = tap;
--- core/index.js
+++ core/index.js
@@ -1,5 +1,5 @@
 const partial = require('./partial/partial');
 const partialRight = require('./partialRight/partialRight');
 
-module.exports = partial;
-module.exports = partialRight;
+module.exports.partial = partial;
+module.exports.partialRight = partialRight;
